1110c87cc5a0efe7e7ec043b3b047294466572c6
MajoLeon06 <majoleonpaz1321@gmail.com>
2026-03-21 22:01:04 -0600

    Reinicio del código, definición de variables y función con solicitud de datos

--- Proyecto 1/Program.cs
+++ Proyecto 1/Program.cs
@@ -1,107 +1,60 @@
-﻿void MostrarMenu()
-{
-    Console.WriteLine("MENÚ\n1:Evaluar nuevo contenido\n2:Mostrar reglas del sistema\n3:Mostrar estadísticas de la sesión\n4:Reiniciar estadísticas\n5:Salir");
-}
-int option = 0;
-int optiontipo = 0;
-double duracion = 0;
-int optionhora = 0;
-int ingresos = 0;
-int opnivel = 0;
-int publicados = 0; //contador
-int rechazados = 0;//contador
-int revision = 0;//contador
-int imalto = 0;//contador impacto
-int immedio = 0;//contador impacto
-int imbajo = 0;//contador impacto
+﻿int option;
+int contenido;
+bool contenidocorrecto;
+double duracion;
+bool duracioncorrecta;
+int clasificacion;
+bool clasificacioncorrecta;
+int hora;
+bool horacorrecta;
+int produccion;
+bool produccioncorrecta;
+int evaluados; //contador total
+int publicados; //contador
+int rechazados; //contador
+int revision; //contador
+int alto; //contador impacto
+int medio; //contador impacto
+int bajo; //contador impacto
 int impactomayor = 0;
-bool correcto;
-string TipoContenido()
+double aprobados;
+int IngresarDatos()
 {
-    Console.WriteLine("OPCIONES:\n1:Película\n2:Serie\n3:Documental\n4:Evento en vivo");
-    Console.WriteLine("Ingrese una opción:");
-    optiontipo = int.Parse(Console.ReadLine());
-    Console.WriteLine("Ingrese duración del contenido en minutos:");
-    duracion = double.Parse(Console.ReadLine());
-    if(optiontipo>=1 && optiontipo<=4 && duracion>0)
-    {
-        if ((optiontipo==1 && (duracion>=60 && duracion<=180)) || (optiontipo==2 && (duracion>=20 && duracion<=90)) || (optiontipo==3 && (duracion>=30 && duracion<=120)) || (optiontipo==4 && (duracion>=30 && duracion<=240)))
-        {
-            return "Válido";
-        }
-        else
-        {
-            return "No cumple con las reglas";
-        }
-    }
-    else
-    {
-        return "No válido";
-    }
-}
-string ClasificacionHora()
-{
-    Console.WriteLine("CLASIFICACIÓN:\n1:Todo público\n2:+13\n3:+18");
-    Console.WriteLine("Ingrese opción:");
-    optionhora = int.Parse(Console.ReadLine());
-    if (optionhora >= 1 && optionhora <= 3)
-    {
-        Console.WriteLine("Ingrese hora programada de transmisión:");
-        int hora = int.Parse(Console.ReadLine());
-        if ((hora >= 0 && hora <= 24) && (optionhora >= 1 && optionhora <= 3))
-        {
-            if (optionhora == 1 || (optionhora == 2 && (hora >= 6 && hora <= 22)) || (optionhora == 3 && (hora >= 22 || hora <= 5)))
-            {
-                return "Válido";
-            }
-            else
-            {
-                return "No cumple con las reglas, realizar ajustes";
-            }
-        }
-        else
-        {
-            return "No válido";
-        }
-        string production = Produccion(optionhora);
-    }
-    else
-    {
-        return "Dato ingresado no válido";
-    }
-    
-}
-string Produccion(int optionhora)
-{
-    Console.WriteLine("Ingrese nivel de producción:\n1:Bajo\n2:Medio\n3:Alto");
-    opnivel = int.Parse(Console.ReadLine());
-    if (((optionhora==1 || optionhora==2) && opnivel==1) || (opnivel==2 || opnivel==3))
-    {
-        return "Válido";
-    }
-    else
-    {
-        return "No válido";
-    }
-}
-
-do
-{
-    MostrarMenu();
     do
     {
-        Console.WriteLine("Ingresar opción:");
-        correcto = int.TryParse(Console.ReadLine(), out option);
-        if(!correcto)
+         Console.Write("Tipo de contenido:\n 1:Película\n 2:Serie\n 3:Documental\n 4:Evento en vivo\nIngresar una opción:");
+         contenidocorrecto = int.TryParse(Console.ReadLine(), out contenido);
+        if (!contenidocorrecto || contenido < 1 || contenido > 4)
         {
-            Console.WriteLine("Opción no válida... intente nuevamente");
+            Console.WriteLine("Dato no válido... intente nuevamente");
         }
-    } while (!correcto);
-    switch (option)
+    } while (!contenidocorrecto || contenido < 1 || contenido > 4);
+    do
     {
-        case 1:
-            TipoContenido();
-            ClasificacionHora();
-            break;
-    }
-} while (option!=6);
+         Console.Write("Ingrese duración en minutos:");
+         duracioncorrecta = double.TryParse(Console.ReadLine(), out duracion);
+        if (!duracioncorrecta)
+        {
+            Console.WriteLine("Dato no válido... intente nuevamente");
+        }
+    } while (!duracioncorrecta);
+    do
+    {
+        Console.Write("Clasificación:\n 1:Todo público\n 2:+13\n 3:+18\nIngresar una opción:");
+        clasificacioncorrecta = int.TryParse(Console.ReadLine(), out clasificacion);
+        if (!clasificacioncorrecta || clasificacion < 1 || clasificacion > 3)
+        {
+            Console.WriteLine("Dato no válido... intente nuevamente");
+        }
+    }while(!clasificacioncorrecta || clasificacion<1 || clasificacion>3);
+    do
+    {
+        Console.WriteLine("Ingrese la hora de transmisión programada (0-23):");
+        horacorrecta = int.TryParse(Console.ReadLine(), out hora);
+        if (!horacorrecta || hora < 0 || hora > 23)
+        {
+            Console.WriteLine("Dato no válido... intente nuevamente");
+        }
+    } while (!horacorrecta || hora < 0 || hora > 23);
+    
+}
